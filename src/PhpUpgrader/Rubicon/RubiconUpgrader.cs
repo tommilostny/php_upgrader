ca5b5739667feac09c3badebcc33cf139ab13d11
tommilostny <tommilostny@live.com>
2022-08-30 16:22:44 +0200

    Found another optional parameter preceeding required parameters..

--- src/PhpUpgrader/Rubicon/RubiconUpgrader.cs
+++ src/PhpUpgrader/Rubicon/RubiconUpgrader.cs
@@ -38,7 +38,7 @@ public sealed class RubiconUpgrader : MonaUpgrader
                             .UpgradeLibDbMysql()
                             .UpgradeArrayMissingKeyValue()
                             .UpgradePiwikaLibsPearRaiseError()
-                            .UpgradeClassMcBalikovna()
+                            .UpgradeRequiredParameterFollowsOptional()
         };
     }
 }
---
+++ src/PhpUpgrader/Rubicon/UpgradeExtensions/RequiredParameterFollowsOptional.cs
@@ -0,0 +1,30 @@
+﻿namespace PhpUpgrader.Rubicon.UpgradeExtensions;
+
+public static class RequiredParameterFollowsOptional
+{
+    /// <summary>
+    /// PHPStan: Deprecated in PHP 8.0: Required parameter ${...} follows optional parameter $domain.
+    /// </summary>
+    public static FileWrapper UpgradeRequiredParameterFollowsOptional(this FileWrapper file)
+    {
+        foreach (var (filename, find, replace) in UpgradableFiles())
+        {
+            if (file.Path.EndsWith(filename, StringComparison.Ordinal))
+            {
+                file.Content.Replace(find, replace);
+            }
+        }
+        return file;
+    }
+
+    private static IEnumerable<(string filename, string find, string replace)> UpgradableFiles()
+    {
+        yield return (filename: Path.Join("classes", "McBalikovna.class.php"),
+                      find:     "public function __construct($domain = 1, $hostname, $username, $password, $database, $connport)",
+                      replace:  "public function __construct($domain, $hostname, $username, $password, $database, $connport)");
+
+        yield return (filename: Path.Join("DataTable", "Filter", "AddColumnsProcessedMetricsGoal.php"),
+                      find:     "public function __construct($table, $enable = true, $processOnlyIdGoal)",
+                      replace:  "public function __construct($table, $enable, $processOnlyIdGoal)");
+    }
+}
