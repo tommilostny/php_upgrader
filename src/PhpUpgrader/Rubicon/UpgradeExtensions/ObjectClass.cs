1eb83926190574215380d3f69cea8f0046f43aef
tommilostny <tommilostny@live.com>
2022-09-02 11:06:17 +0200

    Updated "Object" class upgrade. It became more common.

--- src/PhpUpgrader/Rubicon/UpgradeExtensions/ObjectClass.cs
+++ src/PhpUpgrader/Rubicon/UpgradeExtensions/ObjectClass.cs
@@ -3,33 +3,58 @@
 public static class ObjectClass
 {
     private static bool? _containsObjectClass;
+    private static readonly string _objectFileName = $"{Path.DirectorySeparatorChar}Object.php";
 
-    /// <summary> Aktualizace třídy Object => ObjectBase. Provádí se pouze pokud existuje soubor classes\Object.php. </summary>
+    /// <summary>
+    /// Aktualizace třídy Object => ObjectBase.
+    /// "Object" je v novějším PHP rezervované slovo.
+    /// Provádí se pouze pokud existuje nějaký soubor ".../Object.php".
+    /// </summary>
     /// <remarks> + extends Object, @param Object, @property Object </remarks>
     public static FileWrapper UpgradeObjectClass(this FileWrapper file, RubiconUpgrader upgrader)
     {
-        if (!(_containsObjectClass ??= File.Exists(Path.Join(upgrader.BaseFolder, "weby", upgrader.WebName, "classes", "Object.php"))))
+        if (!(_containsObjectClass ??= LookForObjectPhpFile(upgrader)))
         {
             return file;
         }
-        if (file.Path.EndsWith(Path.Join("classes", "Object.php"), StringComparison.Ordinal) && file.Content.Contains("abstract class Object"))
+        if (file.Path.EndsWith(_objectFileName, StringComparison.Ordinal) && file.Content.Contains("class Object"))
         {
-            file.Content.Replace("abstract class Object", "abstract class ObjectBase");
+            file.Content.Replace("class Object", "class ObjectBase");
             var content = file.Content.ToString();
             var updated = Regex.Replace(content,
-                                        @"function\s+Object\s*\(",
+                                        @"function\s+Object\s*?\(",
                                         "function ObjectBase(",
                                         RegexOptions.None,
                                         TimeSpan.FromSeconds(4));
-            
+
             file.Content.Replace(content, updated);
-            file.MoveOnSavePath = file.Path.Replace(Path.Join("classes", "Object.php"),
-                                                    Path.Join("classes", "ObjectBase.php"),
-                                                    StringComparison.Ordinal);
+            file.MoveOnSavePath = file.Path.Replace(_objectFileName, $"{Path.DirectorySeparatorChar}ObjectBase.php", StringComparison.Ordinal);
         }
         file.Content.Replace("extends Object", "extends ObjectBase")
                     .Replace("@param Object", "@param ObjectBase")
-                    .Replace("@property  Object", "@property  ObjectBase");
+                    .Replace("@property  Object", "@property  ObjectBase")
+                    .Replace("\\Object.php'", "\\ObjectBase.php'")
+                    .Replace("/Object.php'", "/ObjectBase.php'")
+                    .Replace("\\Object.php\"", "\\ObjectBase.php\"")
+                    .Replace("/Object.php\"", "/ObjectBase.php\"")
+                    .Replace("parent::Object", "parent::ObjectBase");
         return file;
+    }
+
+    private static bool LookForObjectPhpFile(RubiconUpgrader upgrader)
+    {
+        return RecursiveSearch(Path.Join(upgrader.BaseFolder, "weby", upgrader.WebName));
+
+        static bool RecursiveSearch(string dir)
+        {
+            foreach (var subdir in Directory.GetDirectories(dir))
+            {
+                if (RecursiveSearch(subdir))
+                {
+                    return true;
+                }
+            }
+            return Directory.GetFiles(dir, "*.php").Any(f => f.EndsWith(_objectFileName, StringComparison.Ordinal));
+        }
     }
 }
