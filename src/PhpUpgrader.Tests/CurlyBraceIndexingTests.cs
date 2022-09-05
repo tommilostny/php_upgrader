86522c4af9b7fbc5d360a8478c3d392fcb8b74a9
tommilostny <tommilostny@live.com>
2022-09-05 12:42:11 +0200

    Added deprecated curly bracket indexing upgrade.

---
+++ src/PhpUpgrader.Tests/CurlyBraceIndexingTests.cs
@@ -0,0 +1,28 @@
+﻿using PhpUpgrader.Mona.UpgradeExtensions;
+
+namespace PhpUpgrader.Tests;
+
+public class CurlyBraceIndexingTests : UnitTestWithOutputBase
+{
+    public CurlyBraceIndexingTests(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    [Theory]
+    [InlineData("\t\t\t\t\tif ($sublen == 6) {\r\n\t\t\t\t\t\t$t = bcmul(''.ord($code{0}), '1099511627776');\r\n\t\t\t\t\t\t$t = bcadd($t, bcmul(''.ord($code{1}), '4294967296'));\r\n\t\t\t\t\t\t$t = bcadd($t, bcmul(''.ord($code{2}), '16777216'));\r\n\t\t\t\t\t\t$t = bcadd($t, bcmul(''.ord($code{3}), '65536'));\r\n\t\t\t\t\t\t$t = bcadd($t, bcmul(''.ord($code{4}), '256'));\r\n\t\t\t\t\t\t$t = bcadd($t, ''.ord($code{5}));\r\n\t\t\t\t\t\tdo {\r\n\t\t\t\t\t\t\t$d = bcmod($t, '900');\r\n\t\t\t\t\t\t\t$t = bcdiv($t, '900');\r\n\t\t\t\t\t\t\tarray_unshift($cw, $d);\r\n\t\t\t\t\t\t} while ($t != '0');\r\n\t\t\t\t\t} else {\r\n\t\t\t\t\t\tfor ($i = 0; $i < $sublen; ++$i) {\r\n\t\t\t\t\t\t\t$cw[] = ord($code{$i});\r\n\t\t\t\t\t\t}\r\n\t\t\t\t\t}\r\n\t\t\t\t\t$code = $rest;")]
+    public void UpgradesValidFile(string content)
+    {
+        //Arrange
+        var file = new FileWrapper("file.php", content);
+
+        //Act
+        file.UpgradeCurlyBraceIndexing();
+
+        //Assert
+        _output.WriteLine(content);
+        _output.WriteLine("=========================================================");
+        var updated = file.Content.ToString();
+        _output.WriteLine(updated);
+        Assert.True(file.IsModified);
+    }
+}
---
+++ src/PhpUpgrader/Mona/UpgradeExtensions/CurlyBraceIndexing.cs
@@ -0,0 +1,29 @@
+﻿namespace PhpUpgrader.Mona.UpgradeExtensions;
+
+public static class CurlyBraceIndexing
+{
+    /// <summary>
+    /// PhpStorm:
+    /// Curly brace access syntax is deprecated since PHP 7.4
+    /// </summary>
+    public static FileWrapper UpgradeCurlyBraceIndexing(this FileWrapper file)
+    {
+        var evaluator = new MatchEvaluator(CurlyToSquareBracketsIndex);
+        var content = file.Content.ToString();
+        var updated = Regex.Replace(content,
+                                    @"(?<array>\$\w+?)\s?{(?<index>.*?)}",
+                                    evaluator,
+                                    RegexOptions.ExplicitCapture | RegexOptions.Compiled,
+                                    TimeSpan.FromSeconds(4));
+
+        file.Content.Replace(content, updated);
+        return file;
+    }
+
+    private static string CurlyToSquareBracketsIndex(Match match)
+    {
+        var array = match.Groups["array"];
+        var index = match.Groups["index"];
+        return $"{array}[{index}]";
+    }
+}
