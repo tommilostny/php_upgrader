5f09b5e28e8efa4f491bcad1a89355037bd8d832
tommilostny <tommilostny@live.com>
2022-09-13 13:55:12 +0200

    Updated FTP sync error handling. Added ability to skip files with errors and printing the error message to the screen.

--- src/FtpUpdateChecker/FtpOperations/SynchronizableFtpOperation.cs
+++ src/FtpUpdateChecker/FtpOperations/SynchronizableFtpOperation.cs
@@ -8,9 +8,10 @@ internal abstract class SynchronizableFtpOperation : FtpOperation
     protected SynchronizableFtpOperation(string username, string password, string hostname) : base(username, password, hostname)
     {
         _session.FileTransferred += FileTransfered;
+        _session.QueryReceived += QueryReceived;
     }
 
-    public void Synchronize(string path, string baseFolder, string webName, SynchronizationMode synchronizationMode, string startMessage)
+    protected void Synchronize(string path, string baseFolder, string webName, SynchronizationMode synchronizationMode, string startMessage)
     {
         TryOpenSession();
 
@@ -19,27 +20,32 @@ internal abstract class SynchronizableFtpOperation : FtpOperation
         Console.ResetColor();
         try
         {
+            var transferOptions = new TransferOptions
+            {
+                FileMask = "*.php"
+            };
             var result = _session.SynchronizeDirectories(
                 synchronizationMode,
                 localPath: Path.Join(baseFolder, "weby", webName),
                 remotePath: path,
                 removeFiles: false,
                 mirror: true,
-                options: new TransferOptions { FileMask = "*.php" }
+                options: transferOptions
             );
             result.Check();
-            PrintSyncSuccess(synchronizationMode, result);
+            PrintSyncResult(synchronizationMode, result);
         }
         catch (Exception ex)
         {
             Console.Write("\r❌ ");
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.Error.WriteLine("Při nahrávání souborů nastala chyba:");
+            Console.Error.WriteLine($"Při nahrávání souborů nastala chyba {ex.GetType().Name}:");
             Console.Error.WriteLine(ex.Message);
             Console.ResetColor();
         }
     }
 
+    /// <summary> Událost, která nastane při přenosu souboru jako součást metod stahování a nahrávání. </summary>
     private void FileTransfered(object sender, TransferEventArgs e)
     {
         switch (e)
@@ -57,7 +63,20 @@ internal abstract class SynchronizableFtpOperation : FtpOperation
         }       
     }
 
-    private static void PrintSyncSuccess(SynchronizationMode synchronizationMode, SynchronizationResult result)
+    /// <summary> Událost, která nastane, když je potřeba rozhodnutí (tj. typicky u jakékoli nezávažné chyby). </summary>
+    private void QueryReceived(object sender, QueryReceivedEventArgs e)
+    {
+        if (!e.Message.StartsWith("Lost connection."))
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("\r❌ ");
+            Console.WriteLine(e.Message.Replace("\n", "\r\n   ").TrimEnd());
+            Console.ResetColor();
+        }
+        e.Continue();
+    }
+
+    private static void PrintSyncResult(SynchronizationMode synchronizationMode, SynchronizationResult result)
     {
         Console.WriteLine("\nProces dokončen.");
         switch (synchronizationMode)
