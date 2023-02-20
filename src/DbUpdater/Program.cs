6e83615523995ecb222345a49580d7196c28d6b9
tommilostny <tommilostny@live.com>
2023-02-20 20:07:09 +0100

    Added project that loads postgre dump and comments unwanted lines that break the import (possibly future whole db updater without needing the Adminer web interface)

---
+++ src/DbUpdater/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+string webName;
+try
+{
+    webName = args.First();
+}
+catch (Exception)
+{
+    Console.Error.WriteLine("Please provide a web name as a program argument.");
+    return;
+}
+
+var evaluator = new MatchEvaluator(match => $"--{match.Value.Replace("\n", "\n--")}");
+
+foreach (var file in Directory.EnumerateFiles("/McRAI/dumps/").Where(f => Regex.IsMatch(f, $@"[^\\/]*?{webName}(.(?!_UPDATED))*?\.sql")))
+{
+    var content = File.ReadAllText(file);
+    File.WriteAllText(file.Replace(".sql", "_UPDATED.sql"), SchemaRegex().Replace(content, evaluator));
+}
+
+partial class Program
+{
+    [GeneratedRegex(".*?(SCHEMA|(FUNCTION(.|\n)*?;))", RegexOptions.ExplicitCapture)]
+    private static partial Regex SchemaRegex();
+}
