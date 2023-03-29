bfb31cd6347ef927351d59cd35ddbb88cd8c7848
Tom Milostný <tommilostny@live.com>
2023-03-29 20:15:44 +0200

    Added possible replace for drop/create on views (that for some reason are crated as tables by the dump)

--- src/DbUpdater/Program.cs
+++ src/DbUpdater/Program.cs
@@ -11,23 +11,37 @@ catch
     return;
 }
 
-var schemaEvaluator = new MatchEvaluator(match => $"--{match.Value.Replace("\n", "\n--")}");
-var dropEvaluator = new MatchEvaluator(match => $"DROP{match.Groups["what"]} CASCADE;");
+var schemaEval = new MatchEvaluator
+(
+    match => $"--{match.Value.Replace("\n", "\n--")}"
+);
+var dropEval = new MatchEvaluator
+(
+    match => $"DROP{match.Groups["what"]} CASCADE;"
+);
+var dropCreateEval = new MatchEvaluator
+(
+    match => match.Groups["w"].Value == "TABLE" ? $"{match.Groups["s"]}VIEW{match.Groups["e"]}" : match.Value
+);
 
 foreach (var file in Directory.EnumerateFiles("/McRAI/dumps/").Where(f => Regex.IsMatch(f, $@"[^\\/]*?{webName}(.(?!_UPDATED))*?\.sql")))
 {
     var content = File.ReadAllText(file).Replace("\\connect", "--\\connect");
-    content = DropRegex().Replace(content, dropEvaluator);
-    content = SchemaRegex().Replace(content, schemaEvaluator);
+    //content = DropCreateRegex().Replace(content, dropCreateEval);
+    content = DropRegex().Replace(content, dropEval);
+    content = SchemaRegex().Replace(content, schemaEval);
 
     File.WriteAllText(file.Replace(".sql", "_UPDATED.sql"), content);
 }
 
 partial class Program
 {
-    [GeneratedRegex(".*?(SCHEMA|(FUNCTION(?!\\w)(.|\n)*?;))", RegexOptions.ExplicitCapture)]
+    [GeneratedRegex(@".*?(SCHEMA|(FUNCTION(?!\w)(.|\n)*?;))", RegexOptions.ExplicitCapture)]
     private static partial Regex SchemaRegex();
 
     [GeneratedRegex("DROP(?<what>(.(?!CASCADE))+?);", RegexOptions.ExplicitCapture)]
     private static partial Regex DropRegex();
+
+    [GeneratedRegex(@"(?<s>DROP VIEW(.|\n)*?CREATE\s)(?<w>\w+?)(?<e>\s(.|\n)*?;)", RegexOptions.ExplicitCapture)]
+    private static partial Regex DropCreateRegex();
 }
