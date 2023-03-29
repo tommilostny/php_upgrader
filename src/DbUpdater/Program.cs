0657853547aaf80722773b65b659505c34804bd9
Tom Milostný <tommilostny@live.com>
2023-03-29 19:54:24 +0200

    Updated (db dump update) to change drop commands not using cascade.

--- src/DbUpdater/Program.cs
+++ src/DbUpdater/Program.cs
@@ -5,22 +5,29 @@ try
 {
     webName = args.First();
 }
-catch (Exception)
+catch
 {
     Console.Error.WriteLine("Please provide a web name as a program argument.");
     return;
 }
 
-var evaluator = new MatchEvaluator(match => $"--{match.Value.Replace("\n", "\n--")}");
+var schemaEvaluator = new MatchEvaluator(match => $"--{match.Value.Replace("\n", "\n--")}");
+var dropEvaluator = new MatchEvaluator(match => $"DROP{match.Groups["what"]} CASCADE;");
 
 foreach (var file in Directory.EnumerateFiles("/McRAI/dumps/").Where(f => Regex.IsMatch(f, $@"[^\\/]*?{webName}(.(?!_UPDATED))*?\.sql")))
 {
-    var content = File.ReadAllText(file);
-    File.WriteAllText(file.Replace(".sql", "_UPDATED.sql"), SchemaRegex().Replace(content, evaluator));
+    var content = File.ReadAllText(file).Replace("\\connect", "--\\connect");
+    content = DropRegex().Replace(content, dropEvaluator);
+    content = SchemaRegex().Replace(content, schemaEvaluator);
+
+    File.WriteAllText(file.Replace(".sql", "_UPDATED.sql"), content);
 }
 
 partial class Program
 {
-    [GeneratedRegex(".*?(SCHEMA|(FUNCTION(.|\n)*?;))", RegexOptions.ExplicitCapture)]
+    [GeneratedRegex(".*?(SCHEMA|(FUNCTION(?!\\w)(.|\n)*?;))", RegexOptions.ExplicitCapture)]
     private static partial Regex SchemaRegex();
+
+    [GeneratedRegex("DROP(?<what>(.(?!CASCADE))+?);", RegexOptions.ExplicitCapture)]
+    private static partial Regex DropRegex();
 }
