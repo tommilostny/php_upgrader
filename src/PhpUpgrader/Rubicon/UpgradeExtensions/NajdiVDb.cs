17f010c4c7f54c98bdc804516e199006c8618756
Tom Milostný <tommilostny@live.com>
2023-07-26 11:35:10 +0200

    Updated added if checks in najdi_v_db.

--- src/PhpUpgrader/Rubicon/UpgradeExtensions/NajdiVDb.cs
+++ src/PhpUpgrader/Rubicon/UpgradeExtensions/NajdiVDb.cs
@@ -16,15 +16,18 @@ public static partial class NajdiVDb
         return file;
     }
 
-    [GeneratedRegex(@"\$vysledek\s?=\s?(?<condVar>\$\w+?)\[(.|\n)*?return\s?\(?\$vysledek\)?;.*?\n", RegexOptions.ExplicitCapture, matchTimeoutMilliseconds: 66666)]
+    [GeneratedRegex(@"(?<indent>[ \t]*)(?<line1>(?<var1>\$\w+?)\s?=\s?pg_query.*?;)[^}]*?(?<line2>(?<var2>\$\w+?)\s?=\s?pg_fetch_assoc.*?;)[^}]*?(?<result>\$vysledek\s?=\s?(\k<var2>)\[(.|\n)*?return\s?\(?\$vysledek\)?;.*?\n)", RegexOptions.ExplicitCapture, matchTimeoutMilliseconds: 66666)]
     private static partial Regex VysledekRegex();
 
-    private static MatchEvaluator _wrapInIfEval = new
-    (
-        match => $@"if ({match.Groups["condVar"]} !== false) {{
-        {match.Value.Replace("\treturn", "        return", StringComparison.Ordinal).Replace("//return", "    //return", StringComparison.Ordinal)}    }} else {{
-        // Žádné výsledky nebyly nalezeny, můžete vrátit například null nebo jinou vhodnou hodnotu.
-        return """";
-    }}
-");
+    private static MatchEvaluator _wrapInIfEval = new(match =>
+    {
+        var s = match.Groups["indent"].Value;
+        var line1 = match.Groups["line1"].Value;
+        var line2 = match.Groups["line2"].Value;
+        var var1 = match.Groups["var1"].Value;
+        var var2 = match.Groups["var2"].Value;
+        var result = match.Groups["result"].Value;
+
+        return $"{s}{line1}\n{s}if ({var1} === false) {{\n{s}\treturn \"\";\n{s}}}\n{s}{line2}\n{s}if ({var2} === false) {{\n{s}\treturn \"\";\n{s}}}\n{s}{result}";
+    });
 }
