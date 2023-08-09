05f2f6030727c9eece35617e37a498d79eb6556d
Tom Milostný <tommilostny@live.com>
2023-08-09 16:26:59 +0200

    Updated undefined const access on array.

--- src/PhpUpgrader/Mona/UpgradeExtensions/UndefinedConstAccess.cs
+++ src/PhpUpgrader/Mona/UpgradeExtensions/UndefinedConstAccess.cs
@@ -27,9 +27,17 @@ public static partial class UndefinedConstAccess
 
         static string _FixInvalidConstAccessInString(Match stringMatch)
         {
+            if (!stringMatch.Groups["str"].Success || stringMatch.Value.Contains("<?php", StringComparison.OrdinalIgnoreCase))
+            {
+                return stringMatch.Value;
+            }
             using var sb = ZString.CreateStringBuilder();
             var i = 0;
-            foreach (var varMatch in UndefinedConstAccessRegex().Matches(stringMatch.Value).Cast<Match>())
+            var invalidVarAccesses = InvalidAccessInStringRegex()
+                .Matches(stringMatch.Value)
+                .Where(m => m.Groups["inv"].Success);
+
+            foreach (var varMatch in invalidVarAccesses)
             {
                 sb.Append(stringMatch.ValueSpan[i..varMatch.Index]);
                 sb.Append('"');
@@ -44,167 +52,12 @@ public static partial class UndefinedConstAccess
         }
     }
 
-    [GeneratedRegex(@"(?<var>\$\w+?\S*?)\[(?<notConst>[a-z_]+?)\]", RegexOptions.ExplicitCapture, matchTimeoutMilliseconds: 55555)]
+    [GeneratedRegex(@"(?<var>(?<!\\|\/\/.*?)\$\w+?[^\s.&@\]""']*?)\[(?<notConst>[a-z_][a-z_0-9]*?)\]", RegexOptions.ExplicitCapture, matchTimeoutMilliseconds: 55555)]
     private static partial Regex UndefinedConstAccessRegex();
 
-    [GeneratedRegex(@"(?<=<\?(([pP][hH][pP])|=)((.|\n)(?!(\?>|\/(\/|\*(?!.*?\*\/)))))*?)""([^""]|(?<=(?<!\\)\\)"")*?(?<!{)\$\w+?\S*?\['[a-z_]+?'\](?!}).*?""", RegexOptions.ExplicitCapture, matchTimeoutMilliseconds: 55555)]
-    private static partial Regex InvalidUseInStringRegex();
+    [GeneratedRegex(@"""\.\$\w+?[^\s.&@\]""']*?\['[a-z_][a-z_0-9]*?'\]\.""|(?<inv>\$\w+?[^\s.&@\]""']*?\['[a-z_][a-z_0-9]*?'\])", RegexOptions.ExplicitCapture, matchTimeoutMilliseconds: 55555)]
+    private static partial Regex InvalidAccessInStringRegex();
 
-    ///// <summary>
-    ///// $mail['telo'] = "Děkujeme za Vaši poptávku, <br /><br />formulář obsahuje tyto položky: <br />".$mail['vybava']."\n\n<br /> e-mail: ".$mail['email']."\n\n";
-    ///// $mail['wadresa'] = "formular.set@vestavne-spotrebice.cz, spotrebice@mcrai.eu, $mail['email']";
-    ///// $mail['predmet'] = "poptavka na set - vestavne-spotrebice.cz";
-    ///// 
-    ///// $mail['odesilatel']  = "formular.set@vestavne-spotrebice.cz";
-    ///// 
-    ///// $hlavicka = "From: $mail['odesilatel']\r\n";
-    ///// $hlavicka.= "Reply-To: $mail['odesilatel']\r\n";
-    ///// $hlavicka.="Content-Type: text/html; charset=utf-8\n";	
-    ///// $hlavicka.= "X-Mailer: PHP";
-    ///// 
-    ///// mail("$mail['wadresa']", "$mail['predmet']", "$mail['telo']", "$hlavicka")
-    ///// or die("error");
-    ///// </summary>
-    //private static void FixInvalidUseInString(FileWrapper file)
-    //{
-    //    var stringStartIndex = -1;  // "string"
-    //    var inPhpBlock = false;     // <?php code ?>
-    //    var inLineComment = false;  // /* comment */
-    //    var inBlockComment = false; // // comment
-    //    var inSimpleString = false; // 'string'
-    //
-    //    for (var i = 0; i < file.Content.Length; i++)
-    //    {
-    //        if (IsInPhp(file.Content, ref i, ref inPhpBlock) == ReturnState.Continue
-    //            || stringStartIndex == -1 && IsInComment(file.Content, ref i, ref inLineComment, ref inBlockComment) == ReturnState.Continue
-    //            || IsInSimpleString(file.Content, i, ref inSimpleString, ref stringStartIndex) == ReturnState.Continue)
-    //        {
-    //            continue;
-    //        }
-    //        var isQuote = file.Content[i] == '"';
-    //        if (!isQuote || (isQuote && i > 0 && file.Content[i - 1] == '\\'))
-    //        {
-    //            continue;
-    //        }
-    //        if (isQuote)
-    //        {
-    //            if (stringStartIndex == -1 && i < file.Content.Length - 2 && file.Content[i + 1] != '.')
-    //            {
-    //                stringStartIndex = i + 1;
-    //                continue;
-    //            }
-    //            i = _ReplaceInvalidUseInString(i);
-    //            stringStartIndex = -1;
-    //        }
-    //    }
-    //    int _ReplaceInvalidUseInString(in int stringEndIndex)
-    //    {
-    //        var count = stringEndIndex - stringStartIndex;
-    //        Span<char> stringCharsSpan = stackalloc char[count];
-    //        file.Content.CopyTo(stringStartIndex, stringCharsSpan, count);
-    //        /*
-    //        if (file.Path.EndsWith("step_03_old.php", StringComparison.Ordinal))
-    //        {
-    //            Console.WriteLine("===============================================");
-    //            Console.WriteLine(stringCharsSpan.ToString());
-    //            //Console.WriteLine(stringChars);
-    //            //Console.WriteLine("-----------------------------------------------");
-    //            //Console.WriteLine(updated);
-    //            Console.WriteLine("===============================================");
-    //            Console.ReadKey();
-    //        }
-    //        */
-    //        if (stringCharsSpan.Contains('['))
-    //        {
-    //            var stringChars = stringCharsSpan.ToString();
-    //            var updated = InvalidUseInStringRegex().Replace(stringChars, m => $"\".{m.Value}.\"");
-    //
-    //
-    //            file.Content.Replace(stringChars, updated, stringStartIndex, updated.Length);
-    //            return stringStartIndex + updated.Length;
-    //        }
-    //        return stringEndIndex;
-    //    }
-    //}
-    //
-    //private static ReturnState IsInPhp(StringBuilder content, ref int i, ref bool isPhp)
-    //{
-    //    if (!isPhp && i < content.Length - 5)
-    //    {
-    //        isPhp = content[i] == '<' && content[i + 1] == '?'
-    //            && ((content[i + 2] == 'p' && content[i + 3] == 'h' && content[i + 4] == 'p')
-    //                || (content[i + 2] == 'P' && content[i + 3] == 'H' && content[i + 4] == 'P'));
-    //        if (isPhp)
-    //        {
-    //            i += 4;
-    //            return ReturnState.Continue;
-    //        }
-    //    }
-    //    if (isPhp && i < content.Length - 2)
-    //    {
-    //        isPhp = !(content[i] == '?' && content[i + 1] == '>');
-    //        if (!isPhp)
-    //        {
-    //            i += 1;
-    //            return ReturnState.Continue;
-    //        }
-    //    }
-    //    return ReturnState.Ok;
-    //}
-    //
-    //private static ReturnState IsInComment(StringBuilder content, ref int i, ref bool inLineComment, ref bool inBlockComment)
-    //{
-    //    if (!inLineComment && !inBlockComment && content[i] == '/' && i + 1 < content.Length)
-    //    {
-    //        if (content[i + 1] == '/')
-    //        {
-    //            inLineComment = true;
-    //            return ReturnState.Continue;
-    //        }
-    //        if (content[i + 1] == '*')
-    //        {
-    //            inBlockComment = true;
-    //            return ReturnState.Continue;
-    //        }
-    //    }
-    //    if (inLineComment && content[i] == '\n')
-    //    {
-    //        inLineComment = false;
-    //        return ReturnState.Continue;
-    //    }
-    //    if (inBlockComment && content[i] == '*' && i + 1 < content.Length && content[i + 1] == '/')
-    //    {
-    //        inBlockComment = false;
-    //        i++;
-    //        return ReturnState.Continue;
-    //    }
-    //    return inLineComment || inBlockComment ? ReturnState.Continue : ReturnState.Ok;
-    //}
-    //
-    //private static ReturnState IsInSimpleString(StringBuilder content, int i, ref bool inSimpleString, ref int stringStartIndex)
-    //{
-    //    if (stringStartIndex != -1 && inSimpleString)
-    //    {
-    //        inSimpleString = false;
-    //        return ReturnState.Ok;
-    //    }
-    //    if (content[i] == '\'')
-    //    {
-    //        if (inSimpleString && i > 0 && content[i - 1] == '\\')
-    //        {
-    //            return ReturnState.Continue;
-    //        }
-    //        inSimpleString = !inSimpleString;
-    //        return ReturnState.Continue;
-    //    }
-    //    return inSimpleString ? ReturnState.Continue : ReturnState.Ok;
-    //}
-    //
-    //[GeneratedRegex(@"(?<!{|\s?\.\s?|(<\?[pP][hH][pP](.(?!\?>))*?))\$\w+?\['.+?'\](?!}|\s?\.\s?)", RegexOptions.ExplicitCapture, matchTimeoutMilliseconds: 55555)]
-    //private static partial Regex InvalidUseInStringRegex();
-    //
-    //private enum ReturnState
-    //{
-    //    Ok, Continue,
-    //}
+    [GeneratedRegex(@"\/(\/.*?\n|\*(.|\n)*?(\*\/|$))|(?<q>['""](?!<\?[pP][hH][pP]))(((?!\k<q>).|(?<=(?<!\\)\\)\k<q>)(?!\$?\w+?[^\s.&@\]""']*?\['[a-z_][a-z_0-9]*?'\]))*(?<!\\)\k<q>|(?<str>""(?!<\?[pP][hH][pP])([^""\n]|(?<=(?<!\\)\\)"")*(?<!{)\$\w+?[^\s.&@\]""']*?\['[a-z_][a-z_0-9]*?'\](?!})([^""\n]|(?<=(?<!\\)\\)"")*"")", RegexOptions.ExplicitCapture, matchTimeoutMilliseconds: 55555)]
+    private static partial Regex InvalidUseInStringRegex();
 }
