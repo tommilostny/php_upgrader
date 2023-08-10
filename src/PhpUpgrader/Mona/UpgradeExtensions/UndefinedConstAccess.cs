880d514783a3ffb54522e6364bd476d557e66a0b
Tom Milostný <tommilostny@live.com>
2023-08-10 09:19:50 +0200

    Updated uca regex.

--- src/PhpUpgrader/Mona/UpgradeExtensions/UndefinedConstAccess.cs
+++ src/PhpUpgrader/Mona/UpgradeExtensions/UndefinedConstAccess.cs
@@ -58,6 +58,6 @@ public static partial class UndefinedConstAccess
     [GeneratedRegex(@"""\.\$\w+?[^\s.&@\]""']*?\['[a-z_][a-z_0-9]*?'\]\.""|(?<inv>\$\w+?[^\s.&@\]""']*?\['[a-z_][a-z_0-9]*?'\])", RegexOptions.ExplicitCapture, matchTimeoutMilliseconds: 55555)]
     private static partial Regex InvalidAccessInStringRegex();
 
-    [GeneratedRegex(@"\/(\/.*?\n|\*(.|\n)*?(\*\/|$))|(?<q>['""](?!<\?[pP][hH][pP]))(((?!\k<q>).|(?<=(?<!\\)\\)\k<q>)(?!\$?\w+?[^\s.&@\]""']*?\['[a-z_][a-z_0-9]*?'\]))*(?<!\\)\k<q>|(?<str>""(?!<\?[pP][hH][pP])([^""\n]|(?<=(?<!\\)\\)"")*(?<!{)\$\w+?[^\s.&@\]""']*?\['[a-z_][a-z_0-9]*?'\](?!})([^""\n]|(?<=(?<!\\)\\)"")*"")", RegexOptions.ExplicitCapture, matchTimeoutMilliseconds: 55555)]
+    [GeneratedRegex(@"'([^']|(?<=(?<!\\)\\)')*'|(?<str>(?<=<\?(([pP][hH][pP])|=)((.|\n)(?!(\?>|\/(\/|\*(?!.*?\*\/)))))*?)""([^""]|(?<=(?<!\\)\\)"")*(?<![{.])\$\w+?\S*?\['[a-z_]+?'\](?![}.])([^""]|(?<=(?<!\\)\\)"")*"")", RegexOptions.ExplicitCapture, matchTimeoutMilliseconds: 55555)]
     private static partial Regex InvalidUseInStringRegex();
 }
