70c8982fafc1ef867dfd0415b80ea36597348e27
Tom Milostný <tommilostny@live.com>
2023-08-17 16:12:10 +0200

    Updated MPDF: added function urldecode_parts and includes/functions.php path variant.

--- src/PhpUpgrader/Rubicon/UpgradeExtensions/Mpdf.cs
+++ src/PhpUpgrader/Rubicon/UpgradeExtensions/Mpdf.cs
@@ -3,6 +3,7 @@
 public static class Mpdf
 {
     private static readonly string _functionsPath = Path.Join("mpdf", "includes", "functions.php");
+    private static readonly string _functions57Path = Path.Join("MPDF57", "includes", "functions.php");
     private static readonly string _mpdfPath = Path.Join("mpdf", "mpdf.php");
     private static readonly string _mpdf57Path = Path.Join("MPDF57", "mpdf.php");
     private static readonly string _mpdfSourcePath = Path.Join("mpdf", "mpdf_source.php");
@@ -32,12 +33,19 @@ public static class Mpdf
 
     public static FileWrapper UpgradeMpdfFunctions(this FileWrapper file)
     {
-        if (file.Path.EndsWith(_functionsPath, StringComparison.Ordinal))
+        if (file.Path.EndsWith(_functionsPath, StringComparison.Ordinal)
+            || file.Path.EndsWith(_functions57Path, StringComparison.Ordinal))
         {
             file.Content.Replace("$str = preg_replace('/\\&\\#([0-9]+)\\;/me', \"code2utf('\\\\1',{$lo})\",$str);",
                                  "$str = preg_replace_callback(\n\t\t'/\\&\\#([0-9]+)\\;/m',\n\t\tfunction($matches) use ($lo) {\n\t\t\treturn code2utf($matches[1], $lo);\n\t\t},\n\t\t$str\n\t);")
                         .Replace("$str = preg_replace('/\\&\\#x([0-9a-fA-F]+)\\;/me', \"codeHex2utf('\\\\1',{$lo})\",$str);",
                                  "$str = preg_replace_callback(\n\t\t'/\\&\\#x([0-9a-fA-F]+)\\;/m',\n\t\tfunction($matches) use ($lo) {\n\t\t\treturn codeHex2utf($matches[1], $lo);\n\t\t},\n\t\t$str\n\t);");
+            if (!file.Content.Contains("urldecode_parts"))
+            {
+                var endTagIndex = file.Content.LastIndexOf("?>");
+                file.Content.Insert(endTagIndex,
+                    "// mPDF 5.7.4 URLs\nif (!function_exists('urldecode_parts')) {\n  function urldecode_parts($url) {\n\t$file=$url;\n\t$query='';\n\tif (preg_match('/[?]/',$url)) {\n\t\t$bits = preg_split('/[?]/',$url,2);\n\t\t$file=$bits[0];\n\t\t$query='?'.$bits[1];\n\t}\n\t$file = rawurldecode($file);\n\t$query = urldecode($query);\n\treturn $file.$query;\n  }\n}\n");
+            }
         }
         return file;
     }
